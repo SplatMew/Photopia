3944693b2c182a5fb13a9c14d2d36b58de0f0c21
Angel Humberto Torres Cota <angel.torres26@uabc.edu.mx>
2023-05-29 12:44:45 -0700

    changed srvr domain

--- frontend/src/api/posts.js
+++ frontend/src/api/posts.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const getPostsRequests = async() => await axios.get('https://photopia.onrender.com/posts')
+export const getPostsRequests = async() => await axios.get('https://photopia-srv.onrender.com/posts')
 
 export const createPostRequest = async(post) => {
     const form = new FormData()                                
@@ -8,7 +8,7 @@ export const createPostRequest = async(post) => {
         form.append(key, post[key])
     }
 
-    return await axios.post('https://photopia.onrender.com/posts', form,{
+    return await axios.post('https://photopia-srv.onrender.com/posts', form,{
                                         headers:{
                                             "Content-Type": "multipart/form-data"
                                         }
@@ -16,8 +16,8 @@ export const createPostRequest = async(post) => {
 
                                 }
 
-export const deletePostRequest = async id => await axios.delete('https://photopia.onrender.com/posts/' + id)
+export const deletePostRequest = async id => await axios.delete('https://photopia-srv.onrender.com/posts/' + id)
 
-export const getPostRequest = async id => await axios.get('https://photopia.onrender.com/posts/' + id)
+export const getPostRequest = async id => await axios.get('https://photopia-srv.onrender.com/posts/' + id)
 
-export const putPostRequest = async (id, newFields) => await axios.put('https://photopia.onrender.com/posts/' + id, newFields)
+export const putPostRequest = async (id, newFields) => await axios.put('https://photopia-srv.onrender.com/posts/' + id, newFields)
