738173a67646f1d32a8358a709ecb4427334b3b5
Angel Humberto Torres Cota <angel.torres26@uabc.edu.mx>
2023-05-29 22:35:28 -0700

    added more fault tolerance

--- frontend/src/components/errorToast.js
+++ frontend/src/components/errorToast.js
@@ -1,17 +1,20 @@
 import toast from 'react-hot-toast'
-import {navigate} from './newPostCard.js'
+import { useNavigate } from 'react-router-dom'
 
 
 export function errorToast() {
-    toast((t) => (
-        <div>
-          <p>
-            Something went wrong, please. Try again later!
-          </p>
-          <div>
-            <button className='bg-yellow-300 hover:bg-yellow-100 px-3 py-3 text-black rounded-sm mx-2'
-              onClick={() => { navigate('/'); toast.dismiss(t.id); }} >  OK.</button>
-          </div>
-        </div>
-      ))
+
+  const navigate = useNavigate();
+
+  toast((t) => (
+    <div>
+      <p>
+        Something went wrong, please. Try again later!
+      </p>
+      <div>
+        <button className='bg-yellow-300 hover:bg-yellow-100 px-3 py-3 text-black rounded-sm mx-2'
+          onClick={() => { navigate('/'); toast.dismiss(t.id); }} >  OK.</button>
+      </div>
+    </div>
+  ))
 }
